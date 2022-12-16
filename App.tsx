32e429f1eb10134541e10581a2f3fff22a6a409e
Jed Jocson <jed.jocson@obf.ateneo.edu>
2022-12-16 20:07:00 +0800

    First commit.

---
+++ App.tsx
@@ -0,0 +1,54 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type { ReactNode } from 'react';
+import {
+	SafeAreaView,
+	ScrollView,
+	StatusBar,
+	StyleSheet,
+	Text,
+	useColorScheme,
+	View,
+} from 'react-native';
+
+import {
+	Colors,
+	DebugInstructions,
+	Header,
+	LearnMoreLinks,
+	ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App: () => ReactNode = () => {
+	
+
+	return (<></>);
+};
+
+const styles = StyleSheet.create({
+	sectionContainer: {
+		marginTop: 32,
+		paddingHorizontal: 24,
+	},
+	sectionTitle: {
+		fontSize: 24,
+		fontWeight: '600',
+	},
+	sectionDescription: {
+		marginTop: 8,
+		fontSize: 18,
+		fontWeight: '400',
+	},
+	highlight: {
+		fontWeight: '700',
+	},
+});
+
+export default App;
